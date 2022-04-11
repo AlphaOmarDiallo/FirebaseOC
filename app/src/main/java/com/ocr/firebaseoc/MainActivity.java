bdc8b9cfdcc21895ed12562605242eed2f060221
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-04-11 10:26:36 +0200

    Firebase login activity theme set - login email set - snackbar messages set - deprecated method needs to be replaced

--- app/src/main/java/com/ocr/firebaseoc/MainActivity.java
+++ app/src/main/java/com/ocr/firebaseoc/MainActivity.java
@@ -1,12 +1,24 @@
 package com.ocr.firebaseoc;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.UserManager;
+
 import androidx.annotation.Nullable;
 
-import android.os.Bundle;
-
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.snackbar.Snackbar;
 import com.ocr.firebaseoc.databinding.ActivityMainBinding;
 
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
+
+    private static final int RC_SIGN_IN = 9001;
+    UserManager userManager;
 
     @Override
     ActivityMainBinding getViewBinding() {
@@ -16,6 +28,67 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupListeners();
     }
+
+    private void setupListeners() {
+        binding.loginButton.setOnClickListener(view -> {
+            startSignInActivity();
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    private void startSignInActivity() {
+        //Choose authentication providers
+        List<AuthUI.IdpConfig> providers =
+                Collections.singletonList(
+                        new AuthUI.IdpConfig.EmailBuilder().build()
+                );
+
+        // Launch the activity
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo_auth)
+                        .build(),
+                RC_SIGN_IN
+        );
+    }
+
+    private void showSnackBar(String message) {
+        Snackbar.make(binding.mainLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            //SUCCESS
+            if (resultCode == RESULT_OK) {
+                showSnackBar(getString(R.string.connection_succeed));
+            } else {
+                //ERRORS
+                if (response == null) {
+                    showSnackBar(getString(R.string.error_authentication_canceled));
+                } else if (response.getError() != null) {
+                    if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                        showSnackBar(getString(R.string.error_no_internet));
+                    } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                        showSnackBar(getString(R.string.error_unknown_error));
+                    }
+                }
+            }
+        }
+    }
+
 
 }
